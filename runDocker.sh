cd650174cea4aabacf89ed95890f446fff02cf97
Mario-69-Mario <84464653+Mario-69-Mario@users.noreply.github.com>
2023-08-17 20:35:51 -0700

    Update runDocker.sh

--- runDocker.sh
+++ runDocker.sh
@@ -1,10 +1,26 @@
-docker stop code-server2
-docker rm code-server2
+#docker stop code-server2
+#docker rm code-server2
+#mkdir -p ~/.config
+#docker run -dt --name code-server2 -p 0>
+#  -v "$HOME/.config:/home/coder/.config>
+#  -v "$PWD:/home/coder/project" \
+#  -v "/shared:/shared" \
+#  -u "$(id -u):$(id -g)" \
+#  -e "DOCKER_USER=$USER" \
+#  codercom/code-server:latest
+
+
+docker stop code-server-new
+docker rm code-server-new
 mkdir -p ~/.config
-docker run -dt --name code-server2 -p 0>
-  -v "$HOME/.config:/home/coder/.config>
-  -v "$PWD:/home/coder/project" \
+docker run -dt --name code-server-new -p 0.0.0.0:8090:8080 \
+  -v "$HOME/.config:/home/coder/.config" \
+  -v "$HOME:/home/coder/project" \
   -v "/shared:/shared" \
   -u "$(id -u):$(id -g)" \
   -e "DOCKER_USER=$USER" \
+  --restart unless-stopped \
   codercom/code-server:latest
+
+
+docker ps
