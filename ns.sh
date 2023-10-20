074c00d04d321cbd720407a61af4312646f2f171
mario-69-oiram <mario-69-oiram@nel.id.au>
2023-10-20 17:55:10 +0000

    nessa ge

--- ns.sh
+++ ns.sh
@@ -7,3 +7,7 @@ sudo apt install python3-pip
 sudo pip install openai
 sudo pip install requests 
 sudo pip install AudioSegment
+
+git config --global user.name "mario-69-oiram"
+git config --global user.email mario-69-oiram@nel.id.au
+
