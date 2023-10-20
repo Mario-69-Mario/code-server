ade0a10006e5c5617a6e5833107927aa09c65ee3
mario-69-oiram <mario-69-oiram@nel.id.au>
2023-10-20 17:54:49 +0000

    updates

---
+++ ns.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt -y upgrade
+
+sudo apt-get install ffmpeg
+
+sudo apt install python3-pip
+sudo pip install openai
+sudo pip install requests 
+sudo pip install AudioSegment
